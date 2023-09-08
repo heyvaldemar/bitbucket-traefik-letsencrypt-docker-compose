9c00490e9a9c96f328b54db674b296ad575c9479
Vladimir Mikhalev <ask@sre.gg>
2023-09-07 20:00:46 -0400

    Bitbucket with Let's Encrypt Using Docker Compose

--- bitbucket-restore-application-data.sh
+++ bitbucket-restore-application-data.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-BITBUCKET_CONTAINER=$(docker ps -aqf "name=bitbucket_bitbucket")
-BITBUCKET_BACKUPS_CONTAINER=$(docker ps -aqf "name=bitbucket_backups")
+# # bitbucket-restore-application-data.sh Description
+# This script is designed to restore the application data.
+# 1. **Identify Containers**: Similarly to the database restore script, it identifies the service and backups containers by name.
+# 2. **List Application Data Backups**: Displays all available application data backups at the specified backup path.
+# 3. **Select Backup**: Asks the user to copy and paste the desired backup name for application data restoration.
+# 4. **Stop Service**: Stops the service to prevent any conflicts during the restore process.
+# 5. **Restore Application Data**: Removes the current application data and then extracts the selected backup to the appropriate application data path.
+# 6. **Start Service**: Restarts the service after the application data has been successfully restored.
+# To make the `bitbucket-restore-application-data.sh` script executable, run the following command:
+# `chmod +x bitbucket-restore-application-data.sh`
+# By utilizing this script, you can efficiently restore application data from an existing backup while ensuring proper coordination with the running service.
+
+BITBUCKET_CONTAINER=$(docker ps -aqf "name=bitbucket-bitbucket")
+BITBUCKET_BACKUPS_CONTAINER=$(docker ps -aqf "name=bitbucket-backups")
+BACKUP_PATH="/srv/bitbucket-application-data/backups/"
+RESTORE_PATH="/var/atlassian/application-data/bitbucket/"
+BACKUP_PREFIX="bitbucket-application-data"
 
 echo "--> All available application data backups:"
 
-for entry in $(docker container exec -it $BITBUCKET_BACKUPS_CONTAINER sh -c "ls /srv/bitbucket-application-data/backups/")
+for entry in $(docker container exec -it "$BITBUCKET_BACKUPS_CONTAINER" sh -c "ls $BACKUP_PATH")
 do
   echo "$entry"
 done
 
 echo "--> Copy and paste the backup name from the list above to restore application data and press [ENTER]
---> Example: bitbucket-application-data-backup-YYYY-MM-DD_hh-mm.tar.gz"
+--> Example: ${BACKUP_PREFIX}-backup-YYYY-MM-DD_hh-mm.tar.gz"
 echo -n "--> "
 
 read SELECTED_APPLICATION_BACKUP
@@ -19,11 +34,11 @@ read SELECTED_APPLICATION_BACKUP
 echo "--> $SELECTED_APPLICATION_BACKUP was selected"
 
 echo "--> Stopping service..."
-docker stop $BITBUCKET_CONTAINER
+docker stop "$BITBUCKET_CONTAINER"
 
 echo "--> Restoring application data..."
-docker exec -it $BITBUCKET_BACKUPS_CONTAINER sh -c "rm -rf /var/atlassian/application-data/bitbucket/* && tar -zxpf /srv/bitbucket-application-data/backups/$SELECTED_APPLICATION_BACKUP -C /"
+docker exec -it "$BITBUCKET_BACKUPS_CONTAINER" sh -c "rm -rf ${RESTORE_PATH}* && tar -zxpf ${BACKUP_PATH}${SELECTED_APPLICATION_BACKUP} -C /"
 echo "--> Application data recovery completed..."
 
 echo "--> Starting service..."
-docker start $BITBUCKET_CONTAINER
+docker start "$BITBUCKET_CONTAINER"
--- bitbucket-restore-database.sh
+++ bitbucket-restore-database.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-BITBUCKET_CONTAINER=$(docker ps -aqf "name=bitbucket_bitbucket")
-BITBUCKET_BACKUPS_CONTAINER=$(docker ps -aqf "name=bitbucket_backups")
+# # bitbucket-restore-database.sh Description
+# This script facilitates the restoration of a database backup.
+# 1. **Identify Containers**: It first identifies the service and backups containers by name, finding the appropriate container IDs.
+# 2. **List Backups**: Displays all available database backups located at the specified backup path.
+# 3. **Select Backup**: Prompts the user to copy and paste the desired backup name from the list to restore the database.
+# 4. **Stop Service**: Temporarily stops the service to ensure data consistency during restoration.
+# 5. **Restore Database**: Executes a sequence of commands to drop the current database, create a new one, and restore it from the selected compressed backup file.
+# 6. **Start Service**: Restarts the service after the restoration is completed.
+# To make the `bitbucket-restore-database.shh` script executable, run the following command:
+# `chmod +x bitbucket-restore-database.sh`
+# Usage of this script ensures a controlled and guided process to restore the database from an existing backup.
+
+BITBUCKET_CONTAINER=$(docker ps -aqf "name=bitbucket-bitbucket")
+BITBUCKET_BACKUPS_CONTAINER=$(docker ps -aqf "name=bitbucket-backups")
+BITBUCKET_DB_NAME="bitbucketdb"
+BITBUCKET_DB_USER="bitbucketdbuser"
+POSTGRES_PASSWORD=$(docker exec $BITBUCKET_BACKUPS_CONTAINER printenv PGPASSWORD)
+BACKUP_PATH="/srv/bitbucket-postgres/backups/"
 
 echo "--> All available database backups:"
 
-for entry in $(docker container exec -it $BITBUCKET_BACKUPS_CONTAINER sh -c "ls /srv/bitbucket-postgres/backups/")
+for entry in $(docker container exec "$BITBUCKET_BACKUPS_CONTAINER" sh -c "ls $BACKUP_PATH")
 do
   echo "$entry"
 done
@@ -19,13 +35,13 @@ read SELECTED_DATABASE_BACKUP
 echo "--> $SELECTED_DATABASE_BACKUP was selected"
 
 echo "--> Stopping service..."
-docker stop $BITBUCKET_CONTAINER
+docker stop "$BITBUCKET_CONTAINER"
 
 echo "--> Restoring database..."
-docker exec -it $BITBUCKET_BACKUPS_CONTAINER sh -c 'PGPASSWORD="$(echo $POSTGRES_PASSWORD)" dropdb -h postgres -p 5432 bitbucketdb -U bitbucketdbuser \
-&& PGPASSWORD="$(echo $POSTGRES_PASSWORD)" createdb -h postgres -p 5432 bitbucketdb -U bitbucketdbuser \
-&& PGPASSWORD="$(echo $POSTGRES_PASSWORD)" gunzip -c /srv/bitbucket-postgres/backups/'$SELECTED_DATABASE_BACKUP' | PGPASSWORD=$(echo $POSTGRES_PASSWORD) psql -h postgres -p 5432 bitbucketdb -U bitbucketdbuser'
+docker exec "$BITBUCKET_BACKUPS_CONTAINER" sh -c "dropdb -h postgres -p 5432 $BITBUCKET_DB_NAME -U $BITBUCKET_DB_USER \
+&& createdb -h postgres -p 5432 $BITBUCKET_DB_NAME -U $BITBUCKET_DB_USER \
+&& gunzip -c ${BACKUP_PATH}${SELECTED_DATABASE_BACKUP} | psql -h postgres -p 5432 $BITBUCKET_DB_NAME -U $BITBUCKET_DB_USER"
 echo "--> Database recovery completed..."
 
 echo "--> Starting service..."
-docker start $BITBUCKET_CONTAINER
+docker start "$BITBUCKET_CONTAINER"
